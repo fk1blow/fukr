15b1b830e313fe470d54d661332740a960b4dbb0
Dragos Tudorache <hedphug@gmail.com>
2016-01-10 03:17:44 +0200

    add some and none functors?

--- lib/skalex/maybe.ex
+++ lib/skalex/maybe.ex
@@ -8,23 +8,26 @@ defmodule SK.Maybe do
   """
   def from(value), do: {:some, value}
 
+  def some(value), do: from value
+
+  def none(_ \\ nil), do: {:none, nil}
+
   @doc """
   Map over the maybe, applying the value to the provided callback function,
   or does nothing {:none, nil}
   """
-  def map(:none, _callback), do: :none
+  def map({:none, _}, _callback), do: {:none, nil}
   def map({:some, value}, callback), do: SK.Maybe.from(callback.(value))
 
   @doc """
   Transforms into a plain value if {:none, nil}, or simply returns the some
   """
-  def or_else(:none, callback), do: callback.(:none)
+  def or_else({:none, _}, callback), do: callback.({:none, nil})
   def or_else({:some, value}, _callback), do: {:some, value}
 
   @doc """
   Extract the value of the `Maybe`
   """
   def get({:some, value}), do: value
-  def get(:none), do: :none
-  def get(value), do: value
+  def get({:none, _}), do: nil
 end
--- test/skalex_test.exs
+++ test/skalex_test.exs
@@ -4,45 +4,39 @@ defmodule SKTest do
 
   doctest SK
 
-  test "should return a :none, if i create a Maybe with a nil" do
-    res = Maybe.from(nil) |> Maybe.get()
-    assert res == :none
+  test "should return {:some, value}" do
+    res = Maybe.from("my value")
+    assert res == {:some, "my value"}
   end
 
-  test "should not run the function, if i create a Maybe with a nil" do
-    res = Maybe.from(nil) |> Maybe.map(fn _ -> true end)
-    assert res == :none
-  end
-
-  test "should run `or_else` instead of `map`, if i create a Maybe with a nil" do
-    res = Maybe.from(nil)
-    |> Maybe.map(fn _ -> true end)
-    |> Maybe.or_else(fn val -> val end)
-    assert res == :none
-  end
-
-  test "should run `map` instead of `or_else`, if i create a Maybe with a value" do
-    res = Maybe.from(true)
-    |> Maybe.map(fn val -> val end)
-    |> Maybe.or_else(fn _ -> false end)
+  test "should create a some" do
+    res = Maybe.some(true)
     assert res == {:some, true}
   end
 
-  test "should run the `map` function, if i create a Maybe with a value" do
-    res = Maybe.from(true) |> Maybe.map(fn val -> val end)
-    assert res == {:some, true}
+  test "should create a :none" do
+    res = Maybe.none()
+    assert res == {:none, nil}
   end
 
-  test "should run the function and return a %{:val => \"the value\"}" do
-    res = Maybe.from(true)
-    |> Maybe.map(fn val -> %{:val => val} end)
-    |> Maybe.get()
-    assert res == %{:val => true}
+  test "should run the map, for some" do
+    res = Maybe.some("something new")
+    |> Maybe.map(fn val -> val <> " and stuff" end)
+    assert res == {:some, "something new and stuff"}
   end
 
-  test "should run the function and return a {:some, %{:val => \"the value\"}}" do
-    res = Maybe.from(true)
-    |> Maybe.map(fn val -> %{:val => val} end)
-    assert res == {:some, %{:val => true}}
+  test "should not run the map, for none" do
+    res = Maybe.none() |> Maybe.map(fn _ -> true end)
+    assert res == {:none, nil}
+  end
+
+  test "should return the value of some" do
+    res = Maybe.some("wooow") |> Maybe.get()
+    assert res == "wooow"
+  end
+
+  test "should return the value of none" do
+    res = Maybe.none() |> Maybe.get()
+    assert res == nil
   end
 end
